47ebb0dd0b5d711afd5122fbf1cb090714f518d3
nc1z <ncxwork@gmail.com>
2023-01-14 11:59:26 +0800

    Initial Setup - Spring Boot

---
+++ simplebot/src/main/java/com/app/simplebot/SimplebotApplication.java
@@ -0,0 +1,13 @@
+package com.app.simplebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebotApplication.class, args);
+	}
+
+}
---
+++ simplebot/src/test/java/com/app/simplebot/SimplebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.simplebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
