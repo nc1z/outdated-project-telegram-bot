23006e110b5bd916142b75b330d41e947ed1364c
nc1z <ncxwork@gmail.com>
2023-01-17 17:07:33 +0800

    [Model] Added QuoteModel

---
+++ simplebot/src/main/java/com/app/simplebot/model/QuoteModel.java
@@ -0,0 +1,49 @@
+package com.app.simplebot.model;
+
+public class QuoteModel {
+    private String q;
+    private String a;
+    private String c;
+    private String h;
+
+    public QuoteModel() {}
+
+    public QuoteModel(String q, String a, String c, String h) {
+        this.q = q;
+        this.a = a;
+        this.c = c;
+        this.h = h;
+    }
+
+    public String getQ() {
+        return q;
+    }
+
+    public void setQ(String q) {
+        this.q = q;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getH() {
+        return h;
+    }
+
+    public void setH(String h) {
+        this.h = h;
+    }
+}
