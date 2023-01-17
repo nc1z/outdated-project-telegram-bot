4c10b7de3b256be24b715e7b14745a459ddaeec4
nc1z <ncxwork@gmail.com>
2023-01-17 17:24:23 +0800

    [Model] Added JokeModel

---
+++ simplebot/src/main/java/com/app/simplebot/model/JokeModel.java
@@ -0,0 +1,49 @@
+package com.app.simplebot.model;
+
+public class JokeModel {
+    private String type;
+    private String setup;
+    private String punchline;
+    private int id;
+
+    public JokeModel() {}
+
+    public JokeModel(String type, String setup, String punchline, int id) {
+        this.type = type;
+        this.setup = setup;
+        this.punchline = punchline;
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSetup() {
+        return setup;
+    }
+
+    public void setSetup(String setup) {
+        this.setup = setup;
+    }
+
+    public String getPunchline() {
+        return punchline;
+    }
+
+    public void setPunchline(String punchline) {
+        this.punchline = punchline;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
